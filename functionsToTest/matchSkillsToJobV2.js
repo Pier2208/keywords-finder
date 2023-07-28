590ca487d50e6c7a8dca75857f283cf2fdf4cc02
Pier2208 <p.leroy.mtl@gmail.com>
2023-07-28 11:38:18 -0400

    modifs

--- functionsToTest/matchSkillsToJobV2.js
+++ functionsToTest/matchSkillsToJobV2.js
@@ -1,6 +1,7 @@
 export function matchSkillsToJobV2(skills, jobDescription) {
   const cache = {};
   const words = jobDescription.split(" ");
+  const regex = /[,()!?:.;"*\n ]$/g;
 
   return skills.filter((skill) => {
     const skillLen = skill.split(" ").length;
@@ -10,16 +11,15 @@ export function matchSkillsToJobV2(skills, jobDescription) {
         cache[skillLen] = words.map((_, i) =>
           words.slice(i, i + skillLen).join(" ")
         );
-        return cache[skillLen].find(
-          (words) => words.toLowerCase() === skill.toLowerCase()
-        );
-      } else {
-        return cache[skillLen].find(
-          (words) => words.toLowerCase() === skill.toLowerCase()
-        );
       }
+      return cache[skillLen].find(
+        (words) =>
+          words.replace(regex, "").toLowerCase() === skill.toLowerCase()
+      );
     } else {
-      return words.find((word) => word.toLowerCase() === skill.toLowerCase());
+      return words.find(
+        (word) => word.replace(regex, "").toLowerCase() === skill.toLowerCase()
+      );
     }
   });
 }
